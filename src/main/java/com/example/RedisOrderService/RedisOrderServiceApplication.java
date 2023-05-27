36353370c98102fb29a670d5c880a0af8d823b18
kym9129 <kym9129@naver.com>
2023-05-28 00:27:15 +0900

    add commit

---
+++ src/main/java/com/example/RedisOrderService/RedisOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.RedisOrderService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RedisOrderService/RedisOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RedisOrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
